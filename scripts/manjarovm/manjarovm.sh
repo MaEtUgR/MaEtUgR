5a0a302dd4765c936157746a3d2953455c8817d4
Matthias Grob <maetugr@gmail.com>
2019-10-10 09:04:25 +0200

    Add Manjaro VM setup script

---
+++ scripts/manjarovm/manjarovm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+sudo pacman -Syu \
+	htop \
+	imwheel \
+	chromium \
+	tk aspell-en \
+	gnome-shell-extension-desktop-icons \
+	;
+
+sudo pacman -R firefox
+
+# configure gnome
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.5 # scale everything
+gsettings set org.gnome.desktop.interface enable-animations false # disable animations
+# dash-to-dock
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM # bottom bar
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64 # big bar
+# stand-by settings
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+# clock settings
+#gsettings set org.gnome.desktop.interface clock-show-weekday false
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
+# desktop icons
+#gnome-shell-extension-tool -e desktop-icons@csoriano
+
+# fix mouse wheel scrolling
+cat <<EOF | install -D /dev/stdin ~/.config/autostart/imwheel.desktop
+[Desktop Entry]
+Type=Application
+Name=imwheel
+Exec=/usr/bin/imwheel --kill
+Comment=Fix mouse scroll wheel issues.
+EOF
+cat > ~/.imwheelrc <<EOF
+".*"
+None,      Up,   Button4, 2
+None,      Down, Button5, 2
+Control_L, Up,   Control_L|Button4
+Control_L, Down, Control_L|Button5
+Shift_L,   Up,   Shift_L|Button4
+Shift_L,   Down, Shift_L|Button5
+EOF
+imwheel --kill
