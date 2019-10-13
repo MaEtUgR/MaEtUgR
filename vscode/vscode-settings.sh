c8005d79e411d3ecb096261abbea889dd6818dcd
Matthias Grob <maetugr@gmail.com>
2019-10-13 10:24:32 +0200

    vscode: add settings download script

---
+++ vscode/vscode-settings.sh
@@ -0,0 +1,7 @@
+pushd ~/.config/Code/User/
+pushd ~/.config/Code\ -\ OSS/User/
+
+wget -N https://raw.githubusercontent.com/MaEtUgR/MaEtUgR/master/vscode/settings.json
+wget -N https://raw.githubusercontent.com/MaEtUgR/MaEtUgR/master/vscode/keybindings.json
+
+popd
