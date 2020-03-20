5c3a3ef28a6d76de3d72da1c9accf6662c56970d
Matthias Grob <maetugr@gmail.com>
2020-03-20 10:44:16 +0100

    docker.sh: add line to build, run and copy log files for CI test

--- scripts/manjaro/docker.sh
+++ scripts/manjaro/docker.sh
@@ -9,3 +9,4 @@ PX4_DOCKER_REPO=px4io/px4-dev-ros-melodic:2020-01-13 ./Tools/docker_run.sh bash
 id # to check correct permissions
 DONT_RUN=1 make px4_sitl gazebo # build sitl gazebo without running it
 ./test/rostest_px4_run.sh mavros_posix_tests_offboard_posctl.test mission:= vehicle:=iris # run SITL offboard test
+DONT_RUN=1 make px4_sitl gazebo && PX4_SIM_SPEED_FACTOR=100 ./test/rostest_px4_run.sh mavros_posix_tests_offboard_posctl.test mission:= vehicle:=iris ;  mv /home/user/.ros/log/*/* .
