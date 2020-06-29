80f6aa98db46a8897d59367a1e759035e13f74c5
Matthias Grob <maetugr@gmail.com>
2020-06-29 21:38:56 +0200

    ssh.sh
    make ssh key generation work in one copy paste command

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,4 +1,4 @@
-ssh-keygen -t rsa -b 4096 -C "maetugr@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "maetugr@gmail.com" && \
 # eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa && \
 cat < ~/.ssh/id_rsa.pub
