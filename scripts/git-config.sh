5513eab0f852c37c38ef3e3350158808b81c3b97
Matthias Grob <maetugr@gmail.com>
2020-07-13 17:05:31 +0200

    git-config: make nano the default git editor

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,3 +1,4 @@
 git config --global user.name "Matthias Grob"
 git config --global user.email "maetugr@gmail.com"
 git config --global merge.conflictstyle diff3
+git config --global core.editor "nano"
