7941c29a8765c6dac548ba6fc20515658ef558ce
Matthias Grob <maetugr@gmail.com>
2021-01-17 12:36:43 +0100

    git-config: add pull fast-forward only

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -2,3 +2,4 @@ git config --global user.name "Matthias Grob"
 git config --global user.email "maetugr@gmail.com"
 git config --global merge.conflictstyle diff3
 git config --global core.editor "nano"
+git config --global pull.ff only
