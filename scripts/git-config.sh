4ed542f6f4bd458f4f4ce76b2447ae6c092f9b70
Matthias Grob <maetugr@gmail.com>
2021-01-25 13:03:37 +0100

    git-config: disable pager for `git branch`

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -3,3 +3,4 @@ git config --global user.email "maetugr@gmail.com"
 git config --global merge.conflictstyle diff3
 git config --global core.editor "nano"
 git config --global pull.ff only
+git config --global pager.branch false
