cc81ac33ef038b071897470ce1d9dba5bf2bf16c
Matthias Grob <maetugr@gmail.com>
2021-02-23 10:02:49 +0100

    Restructure folders

---
+++ scripts/ubuntu/setup.sh
@@ -0,0 +1,5 @@
+pushd ~
+wget -O ubuntuvm.sh https://raw.githubusercontent.com/MaEtUgR/MaEtUgR/master/scripts/ubuntu/ubuntuvm.sh
+chmod +x ubuntuvm.sh
+./ubuntuvm.sh
+popd
--- scripts/ubuntuvm/setup.sh
+++
@@ -1,5 +0,0 @@
-pushd ~
-wget -O ubuntuvm.sh https://raw.githubusercontent.com/MaEtUgR/MaEtUgR/master/scripts/ubuntuvm/ubuntuvm.sh
-chmod +x ubuntuvm.sh
-./ubuntuvm.sh
-popd
