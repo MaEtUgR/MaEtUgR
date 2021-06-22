3e4712a65b940715e142c2a39087186e353e92ab
Matthias Grob <maetugr@gmail.com>
2021-06-22 16:24:15 +0200

    Add oh-my-zsh initial configuration used in WSL

---
+++ scripts/zsh.sh
@@ -0,0 +1,7 @@
+sudo apt install zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+gedit ~/.zshrc
+# ZSH_THEME="agnoster"
+# plugins=(git zsh-autosuggestions)
