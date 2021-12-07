7bb20d2bd132feb526a4f627bad2b847aeb78882
Matthias Grob <maetugr@gmail.com>
2021-12-07 17:48:10 +0100

    zsh.sh: replace gedit with code
    Because I never install all libraries + gedit in WSL

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,7 +2,7 @@ sudo apt install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-gedit ~/.zshrc
+code ~/.zshrc
 # ZSH_THEME="agnoster"
 # plugins=(git zsh-autosuggestions)
 echo "source ~/.profile" >> ~/.zprofile
