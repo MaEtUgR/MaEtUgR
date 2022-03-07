f6fccbeaefe013967b9984f239ba546c6fe015f1
Matthias Grob <maetugr@gmail.com>
2022-03-07 15:54:13 +0100

    git-config: add VS Code GitLens editor

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,6 +1,8 @@
 git config --global user.name "Matthias Grob"
 git config --global user.email "maetugr@gmail.com"
 git config --global merge.conflictstyle diff3
-git config --global core.editor "nano"
+git config --global core.editor "nano" # for terminal only
+git config --global core.editor "code --wait"
+git config --global sequence.editor "code --wait"
 git config --global pull.ff only
 git config --global pager.branch false
