85b4d9dda0994f856a97b72447ddc7211653a7ef
Matthias Grob <maetugr@gmail.com>
2022-12-13 15:08:00 +0100

    scripts: add notes on encrypted linux grub repair

---
+++ scripts/restore-grub-encrypted.sh
@@ -0,0 +1,22 @@
+# derived from https://gist.github.com/samuelcolvin/43c5ed2807e7db004b1058d0c9bfb068
+
+# Boot live media using UEFI and open terminal
+sudo cryptsetup luksOpen /dev/nvme1n1p3 home_nvme # open encrypted luks drive with password
+sudo mount /dev/mapper/home_nvme /mnt # mount root partition
+for i in /dev /proc /sys; do sudo mount -o bind $i /mnt/@$i; done # bind crucial live system folders into root
+chroot /mnt # enter root
+# inside your installations root do
+fdisk -l # to see the partition layout
+# could look like this:
+# ...
+# /dev/nvme0n1p1     2048   1050623   1048576   512M EFI System
+# ...
+# /dev/nvme1n1p1  1050624   2050047    999424   488M Linux filesystem
+# ...
+mount /dev/nvme1n1p1 /boot # mount the boot partition of your linux installation
+mount /dev/nvme0n1p1 /boot/efi # mount the efi partition of your system (can be on a separate SSD that has Windows installed)
+grub-install
+grub-install --recheck
+
+cat /boot/grub/grub.cfg | grep menuentry # check for the entries you have after automatic grub repair, Linux and Windows there?
+# If it's all verified reboot
