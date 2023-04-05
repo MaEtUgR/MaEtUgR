772dd16214bf29c350c954c6577f1e19345f5e03
Matthias Grob <maetugr@gmail.com>
2023-04-05 06:16:06 +0200

    zsh: remove unnecessary command

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,6 +1,5 @@
 sudo apt install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 code ~/.zshrc
 # ZSH_THEME="agnoster"
