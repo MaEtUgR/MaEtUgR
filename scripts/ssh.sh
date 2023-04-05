ee7872bbc919c7e913566c92dc234936ab852b01
Matthias Grob <maetugr@gmail.com>
2023-04-05 06:17:31 +0200

    ssh: remove unnecessary line wrappers

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,4 +1,4 @@
-ssh-keygen -t ed25519 -C "maetugr@gmail.com" && \
+ssh-keygen -t ed25519 -C "maetugr@gmail.com"
 # eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_ed25519 && \
+ssh-add ~/.ssh/id_ed25519
 cat < ~/.ssh/id_ed25519.pub
