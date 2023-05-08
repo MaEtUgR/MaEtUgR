6d54f48086a014f328fca6433089cafe6d038fa4
Matthias Grob <maetugr@gmail.com>
2023-05-08 07:55:41 +0200

    kde: add partitionmanager

--- scripts/arch/kde.sh
+++ scripts/arch/kde.sh
@@ -1,3 +1,3 @@
-sudo pacman -S plasma konsole kate dolphin
+sudo pacman -S plasma konsole kate dolphin partitionmanager
 sudo systemctl enable --now sddm
 sudo systemctl start sddm # to start it only once e.g. on a headless machine
