8dac62f573120ea51fc63416826ddd7db959be1e
Matthias Grob <maetugr@gmail.com>
2023-11-15 10:57:50 +0100

    Ubunut: add fzf commands

---
+++ scripts/ubuntu/fzf.sh
@@ -0,0 +1,4 @@
+sudo apt install fzf
+# keybindings for fuzzy history search
+echo "source /usr/share/doc/fzf/examples/key-bindings.zsh" >> ~/.zshrc
+echo "source /usr/share/doc/fzf/examples/completion.zsh" >> ~/.zshrc
