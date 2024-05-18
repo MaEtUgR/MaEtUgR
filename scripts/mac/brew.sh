41f0a6131f8060f68e61e6e91b2a26d7741be957
Matthias Grob <maetugr@gmail.com>
2024-05-18 11:01:59 +0200

    Create brew.sh

---
+++ scripts/mac/brew.sh
@@ -0,0 +1,3 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/maetugr/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
