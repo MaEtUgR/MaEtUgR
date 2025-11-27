d1517c7acbd934c0a84954589fb508ef260150ce
Matthias Grob <maetugr@gmail.com>
2025-11-27 16:58:29 +0100

    docker.sh: add mavsdk_tests target

--- scripts/arch/docker.sh
+++ scripts/arch/docker.sh
@@ -7,6 +7,6 @@ docker info
 cd Firmware
 PX4_DOCKER_REPO=px4io/px4-dev-simulation-focal:2021-09-08 ./Tools/docker_run.sh bash
 id # to check correct permissions
-DONT_RUN=1 make px4_sitl gazebo # build sitl gazebo without running it
+DONT_RUN=1 make px4_sitl gazebo mavsdk_tests # build sitl gazebo without running it
 ./test/rostest_px4_run.sh mavros_posix_tests_offboard_posctl.test mission:= vehicle:=iris # run SITL offboard test
-DONT_RUN=1 make px4_sitl gazebo && PX4_SIM_SPEED_FACTOR=100 ./test/rostest_px4_run.sh mavros_posix_tests_offboard_posctl.test mission:= vehicle:=iris ;  mv /home/user/.ros/log/*/* .
+DONT_RUN=1 make px4_sitl gazebo mavsdk_tests && PX4_SIM_SPEED_FACTOR=100 ./test/rostest_px4_run.sh mavros_posix_tests_offboard_posctl.test mission:= vehicle:=iris ;  mv /home/user/.ros/log/*/* .
