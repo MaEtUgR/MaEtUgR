74f37dd12d291d346fc53076d03ef820cd4b8123
Matthias Grob <maetugr@gmail.com>
2025-11-27 15:20:00 +0100

    docker.sh: correct PX4 sim container

--- scripts/arch/docker.sh
+++ scripts/arch/docker.sh
@@ -5,7 +5,7 @@ docker info
 
 # PX4
 cd Firmware
-PX4_DOCKER_REPO=px4io/px4-dev-ros-melodic:2020-01-13 ./Tools/docker_run.sh bash
+PX4_DOCKER_REPO=px4io/px4-dev-simulation-focal:2021-09-08 ./Tools/docker_run.sh bash
 id # to check correct permissions
 DONT_RUN=1 make px4_sitl gazebo # build sitl gazebo without running it
 ./test/rostest_px4_run.sh mavros_posix_tests_offboard_posctl.test mission:= vehicle:=iris # run SITL offboard test
